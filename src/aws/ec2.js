4e00ad536782e1a770dd57ab3d3d8f7a178fddb7
Josh Lakenan <joshualakenan@gmail.com>
2024-03-25 15:33:40 -0500

    Added provision_ec2 file to set up security groups
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Alan Cho <alancho397@gmail.com>
    Co-authored-by: Trevor Rees-Jones <treesjones3@gmail.com>

--- src/aws/ec2.js
+++ src/aws/ec2.js
@@ -9,7 +9,7 @@ AWS.config.update({ region: "us-east-2" });
 let ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
 
 let params = {
-  KeyName: "pinniped-test4",
+  KeyName: "pinniped-test",
 };
 
 // Create the key pair     https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ec2-example-key-pairs.html
@@ -18,7 +18,7 @@ ec2.createKeyPair(params, function (err, data) {
     console.log("Error", err);
   } else {
     console.log(JSON.stringify(data));
-    fs.writeFileSync("pinniped-test4.pem", data.KeyMaterial);
+    fs.writeFileSync("pinniped-test.pem", data.KeyMaterial);
   }
 });
 
@@ -29,6 +29,7 @@ let instanceParams = {
   KeyName: "pinniped-test", //name for your instance
   MinCount: 1,
   MaxCount: 1,
+  SecurityGroupIds: ["sg-1234567890abcdef0"],
 };
 
 // Create a promise on an EC2 service object
@@ -69,43 +70,43 @@ instancePromise
     console.error(err, err.stack);
   });
 
-// // Specify parameters for creating a security group
-// let securityGroupParams = {
-//   Description: "Allow SSH traffic",
-//   GroupName: "SSHAccess", // Specify a name for your security group
-// };
+// Specify parameters for creating a security group
+let securityGroupParams = {
+  Description: "Allow SSH traffic",
+  GroupName: "SSHAccess", // Specify a name for your security group
+};
 
-// // Create the security group
-// ec2.createSecurityGroup(securityGroupParams, function (err, data) {
-//   if (err) {
-//     console.log("Error creating security group", err);
-//   } else {
-//     let securityGroupId = data.GroupId;
-//     console.log("Security group created with ID:", securityGroupId);
+// Create the security group
+ec2.createSecurityGroup(securityGroupParams, function (err, data) {
+  if (err) {
+    console.log("Error creating security group", err);
+  } else {
+    let securityGroupId = data.GroupId;
+    console.log("Security group created with ID:", securityGroupId);
 
-//     // Specify parameters for authorizing SSH ingress traffic
-//     let authorizeParams = {
-//       GroupId: securityGroupId,
-//       IpPermissions: [
-//         {
-//           IpProtocol: "tcp",
-//           FromPort: 22, // SSH port
-//           ToPort: 22, // SSH port
-//           IpRanges: [{ CidrIp: "0.0.0.0/0" }], // Allow SSH traffic from any IP address
-//         },
-//       ],
-//     };
+    // Specify parameters for authorizing SSH ingress traffic
+    let authorizeParams = {
+      GroupId: securityGroupId,
+      IpPermissions: [
+        {
+          IpProtocol: "tcp",
+          FromPort: 22, // SSH port
+          ToPort: 22, // SSH port
+          IpRanges: [{ CidrIp: "0.0.0.0/0" }], // Allow SSH traffic from any IP address
+        },
+      ],
+    };
 
-//     // Authorize SSH ingress traffic for the security group
-//     ec2.authorizeSecurityGroupIngress(authorizeParams, function (err, data) {
-//       if (err) {
-//         console.log("Error authorizing security group ingress", err);
-//       } else {
-//         console.log("SSH ingress traffic authorized for security group");
-//       }
-//     });
-//   }
-// });
+    // Authorize SSH ingress traffic for the security group
+    ec2.authorizeSecurityGroupIngress(authorizeParams, function (err, data) {
+      if (err) {
+        console.log("Error authorizing security group ingress", err);
+      } else {
+        console.log("SSH ingress traffic authorized for security group");
+      }
+    });
+  }
+});
 
 /*
 {"KeyFingerprint":"51:b7:4e:07:3a:75:bf:ea:84:70:1a:96:f6:ba:42:54:fd:d4:fc:9f","KeyMaterial":"-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAn6zMuw2rdbnW1vrQdQ/sVv40/66sjQ3yKK3VSnn5czkYcH0E\np5DWTqEeB+bcfgt36V1oWHNTtil+amMCbsPvjqXz1sYx2Tvp0WK59/7GV4lSO5x6\nw7a2FCuphRPo0JjXlAzlNj7O76iAyEg837bs4YNFKB8zXtd5icdxCLbRNTe9e9xU\nuqelnc//P/TlUqjh3W2n45WuL00KlF6320UtLOkG0n66TP8FUIc2gv3rtOL5u0Nn\n9kbFagiOM6LX8XviqIVcQecVleSv/OPH/YXeovYxz1hOqnCosSazYx/l1a/MNZ6i\nvItnolHlDGHE7EJ+41c5SM7CS070ibJ3LI7AvwIDAQABAoIBAEuupM9YOFexgQIe\nmaWSqYvGK6qwyTqGTKw5UKpfS/vwKQRQTV97CAj21vwZ0ZKwruJekrGx4BCsdC3K\nxgFoP2iWgg/fmwsDGd1T9R91k98zyFVW4t1XAdBnrY7VE/mjKDMVNaNTe9QeQwQr\nMSc8VeTByoqs1zVx3jqO3KWruVeGVSYcelFkzA/Gmg/CbXZLYCSIH6WvDoJIA6sK\n0ko1Fga6j7S9HPTqmkyNJW9gN/HFbKah5MpfeDFXpAXNyM05IHk0TDYeI9/JmZhR\nBRiXHguXdYjsqkbSoOIxf9TXbL7WpF8SuRtiPLbvW7N4rAo31jxjAXSwCpNqE00s\nBaToaQECgYEA6vpuKlcHiLG0AHoXlyqDeIPj5dv1WF4Ka2MEseQX1swzN1gqg4R5\naWnRF1WDHbmSLtb5oNUt/S8/Y2tLS2qsm0Qb/kCOtHEihhFKqK1La+51EYLV510Y\nYXvW7UC8Jp9U4g/RJQ6l0d7hhWZocHjRDZqZw8Sq8d5RZWCcchbO0T8CgYEArfW+\nPBN6SjID1UAL02FGukFp4QkDZwn7HMPnJzG8nndRzGr3+LFmgSYM7XyIg/mKaY+z\n/Qqx3CRnXUo6Cj9wwwBBuzYLqT9ZadlQVwDQMAKxoBGuZN3uZ4YfcD2TXEPRePSe\nJKGIMZBoez/58ZsCvc2MARO/vNlPF8hyF+aEsIECgYEAimebwCCm9WMGTUmc35NR\nAQlekSOwLa6moH4gY8+gIwsAF9c8o17CTM7hM8dQhY+qhSBmxM8MWZcc1Sh7nx5k\nMMg7XNUVuT7yQNmqihQY2qgIGJph9/cjxze0bua0bGNpEgaTACrSjP2lBHg/iBAB\nwuuPKnqK3hA/DWGMhL7ebu0CgYEAhxbX1PVXOPjgCyFCTbMz5n3Xx/5k2wZgL/Vd\nsw0nqXOFXRYQXNr+e/pdfasApGQLfXAv2KgOeKL4dfSnX7/mRDgV01jj81mTiORO\nRFIIWdAtud0hLYk+krwP++yFW9f6elCT4mRiTAMVb+i9o0a5+UT+nPOGKRQkHJb3\nKuJDVoECgYBr4et6zTWrf96aIwp6XLNIIJnOjYzOqx35disqy8016+YwvHugn55H\nOCKD7cjWkMrdEtlAp0PAaTftZ0TTkgPrsRmq3MQ/4VkFtWiPe33Sln0nUWkwBkFN\nXVyCRaBpkn90sqX9sEiSqoOySYfuQqxDglHlNyfRKw0IK7bRw+itPA==\n-----END RSA PRIVATE KEY-----","KeyName":"pinniped-test3","KeyPairId":"key-00324cac6d4749041","Tags":[]}
---
+++ src/aws/provision_ec2_v3.js
@@ -0,0 +1,109 @@
+import {
+  EC2Client,
+  CreateSecurityGroupCommand,
+  AuthorizeSecurityGroupIngressCommand,
+  CreateKeyPairCommand,
+  RunInstancesCommand,
+} from "@aws-sdk/client-ec2";
+import { writeFile } from "fs/promises";
+
+// Create EC2 service client
+const ec2Client = new EC2Client({ region: "us-east-2" });
+
+async function createSecurityGroup() {
+  try {
+    // Specify parameters for creating the security group
+    const securityGroupParams = {
+      Description: "Allow SSH, HTTP, and HTTPS traffic",
+      GroupName: "Pinniped-Security3",
+    };
+
+    // Create the security group
+    const { GroupId } = await ec2Client.send(
+      new CreateSecurityGroupCommand(securityGroupParams)
+    );
+    console.log("Security group created with ID:", GroupId);
+
+    // Specify parameters for authorizing ingress traffic
+    const authorizeParams = {
+      GroupId,
+      IpPermissions: [
+        {
+          IpProtocol: "tcp",
+          FromPort: 22,
+          ToPort: 22,
+          IpRanges: [{ CidrIp: "0.0.0.0/0" }], // Allow SSH traffic from any IP address
+        },
+        {
+          IpProtocol: "tcp",
+          FromPort: 80,
+          ToPort: 80,
+          IpRanges: [{ CidrIp: "0.0.0.0/0" }], // Allow HTTP traffic from any IP address
+        },
+        {
+          IpProtocol: "tcp",
+          FromPort: 443,
+          ToPort: 443,
+          IpRanges: [{ CidrIp: "0.0.0.0/0" }], // Allow HTTPS traffic from any IP address
+        },
+        // Add additional ingress rules as needed
+      ],
+    };
+
+    // Authorize ingress traffic for the security group
+    await ec2Client.send(
+      new AuthorizeSecurityGroupIngressCommand(authorizeParams)
+    );
+    console.log("Ingress traffic authorized for security group");
+  } catch (error) {
+    console.error("Error creating security group:", error);
+  }
+}
+
+async function createKeyPair() {
+  try {
+    // Specify parameters for creating the key pair
+    const params = {
+      KeyName: "pinniped-open",
+    };
+
+    // Create the key pair
+    const data = await ec2Client.send(new CreateKeyPairCommand(params));
+    console.log("Key pair created:", data.KeyName);
+
+    // Write the key material to a PEM file
+    await writeFile("pinniped-open.pem", data.KeyMaterial);
+  } catch (error) {
+    console.error("Error creating key pair:", error);
+  }
+}
+
+async function launchInstance(securityGroupId) {
+  try {
+    // Specify parameters for launching the EC2 instance
+    const instanceParams = {
+      ImageId: "ami-0b8b44ec9a8f90422", // AMI ID
+      InstanceType: "t2.nano", // Instance type
+      KeyName: "pinniped-open", // Key pair name
+      MinCount: 1,
+      MaxCount: 1,
+      SecurityGroupIds: [securityGroupId], // Use the created security group ID
+      // Additional parameters as needed
+    };
+
+    // Launch the EC2 instance
+    const data = await ec2Client.send(new RunInstancesCommand(instanceParams));
+    console.log("Created instance:", data.Instances[0].InstanceId);
+  } catch (error) {
+    console.error("Error launching instance:", error);
+  }
+}
+
+// Execute the functions sequentially
+(async () => {
+  await createSecurityGroup();
+  await createKeyPair();
+  // Assuming the security group ID is known after creation, or you can retrieve it from a previous step
+  const securityGroupId = "sg-1234567890"; // Replace with the actual security group ID
+  await launchInstance(securityGroupId);
+})();
