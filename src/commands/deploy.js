4b2711241b5039b3ca9b553362395fce27071b7f
Josh Lakenan <joshualakenan@gmail.com>
2024-03-26 20:30:23 -0500

    UploadFilesToEC2 function working for sending project file structure to ec2
    Co-authored-by: Jonathan Hurd <hurd.jonathan@gmail.com>
    Co-authored-by: Alan Cho <alancho397@gmail.com>
    Co-authored-by: Trevor Rees-Jones <treesjones3@gmail.com>

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -27,45 +27,50 @@ const deploy = async (agrv) => {
   answers.projectName = process.cwd().split("/").pop();
 
   try {
-    //start a loading spinner
-    const spinner = ui.runSpinner(
-      ui.colorStandard(
-        `Provisioning AWS EC2 instance. This may take a few minutes...`
-      )
-    );
+    // //start a loading spinner
+    // const spinner = ui.runSpinner(
+    //   ui.colorStandard(
+    //     `Provisioning AWS EC2 instance. This may take a few minutes...`
+    //   )
+    // );
 
-    // const connectionParams = {
-    //   hostName: "18.227.89.231",
-    //   username: "ubuntu",
-    //   privateKeyPath: "seal6.pem",
-    // };
+    const connectionParams = {
+      hostName: "18.222.27.129",
+      username: "ubuntu",
+      privateKeyPath: "Seals10.pem",
+    };
 
-    const connectionParams = await provision(answers);
-    spinner.text = "Installing node.js on the EC2 instance";
+    // const connectionParams = await provision(answers);
+    // spinner.text = "Installing node.js on the EC2 instance";
 
-    await setFilePermissions(connectionParams.privateKeyPath);
+    // await setFilePermissions(connectionParams.privateKeyPath);
 
-    console.log("Connection Params:", connectionParams);
+    // console.log("Connection Params:", connectionParams);
 
-    const installNodeCmd =
-      "DEBIAN_FRONTEND=noninteractive curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt-get install -y nodejs";
+    // const installNodeCmd =
+    //   "DEBIAN_FRONTEND=noninteractive curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt-get install -y nodejs";
 
-    // Delay for 15 seconds to allow the EC2 instance to be fully provisioned
-    await new Promise((resolve) => setTimeout(resolve, 20000));
+    // // Delay for 15 seconds to allow the EC2 instance to be fully provisioned
+    // await new Promise((resolve) => setTimeout(resolve, 20000));
 
-    // Run a command on the EC2 instance to install Node.js
-    await runCommandOnEC2(connectionParams, installNodeCmd);
+    // // Run a command on the EC2 instance to install Node.js
+    // await runCommandOnEC2(connectionParams, installNodeCmd);
 
-    // Copy the project directory to the EC2 instance
-    spinner.text = "Copying project files to the EC2 instance";
+    // // Copy the project directory to the EC2 instance
+    // spinner.text = "Copying project files to the EC2 instance";
 
     await uploadFilesToEC2(connectionParams);
 
-    spinner.succeed(ui.colorSuccess("Project files copied successfully"));
+    // spinner.succeed(ui.colorSuccess("Project files copied successfully"));
   } catch (err) {
-    console.error("Error copying project directory:", err);
-    spinner.fail(ui.colorError("Project deployment failed"));
+    // console.error("Error copying project directory:", err);
+    console.log(err);
+    // spinner.fail(ui.colorError("Project deployment failed"));
   }
 };
 
 export default deploy;
+
+/*
+ssh -v -i Seals10.pem ubuntu@ec2-18-222-27-129.us-east-2.compute.amazonaws.com
+*/
--- src/utils/uploadFilesToEC2.js
+++ src/utils/uploadFilesToEC2.js
@@ -1,166 +1,158 @@
-// import fs from "fs";
-// import { Client } from "ssh2";
-
-// export default async function uploadFilesToEC2(connectionParams) {
-//   const { hostName, username, privateKeyPath } = connectionParams;
-
-//   const localFilePath = "index.js";
-//   const remoteFilePath = "/home/ubuntu/index.js";
-
-//   // Create SSH connection
-//   const sshClient = new Client();
-//   await new Promise((resolve, reject) => {
-//     sshClient.on("error", reject);
-//     sshClient.on("ready", resolve);
-//     sshClient.connect({
-//       host: hostName,
-//       username: username,
-//       privateKey: fs.readFileSync(privateKeyPath),
-//     });
-//   });
-
-//   // SCP the file to the EC2 instance
-//   await new Promise((resolve, reject) => {
-//     sshClient.sftp((err, sftp) => {
-//       if (err) {
-//         sshClient.end();
-//         return reject(err);
-//       }
-//       sftp.fastPut(localFilePath, remoteFilePath, (err) => {
-//         sshClient.end();
-//         if (err) {
-//           return reject(err);
-//         }
-//         resolve();
-//       });
-//     });
-//   });
-
-//   console.log("File uploaded successfully.");
-// }
-
-// import fs from "fs";
-// import path from "path";
-// import { Client } from "ssh2";
-
-// // Filter function to exclude node_modules and package-lock.json
-// const filterFunc = (fileName) => {
-//   return !["node_modules", "package-lock.json"].includes(fileName);
-// };
-
-// export default async function uploadFilesToEC2(connectionParams) {
-//   const { hostName, username, privateKeyPath } = connectionParams;
-//   const localDir = process.cwd(); // Current working directory
-
-//   // Get list of files and directories in the current working directory
-//   const items = fs.readdirSync(localDir);
-
-//   console.log(items);
-
-//   // Filter out node_modules and package-lock.json
-//   const filesToUpload = items.filter(filterFunc);
-
-//   console.log(filesToUpload);
-
-//   // Create SSH connection
-//   const sshClient = new Client();
-//   await new Promise((resolve, reject) => {
-//     sshClient.on("error", reject);
-//     sshClient.on("ready", resolve);
-//     sshClient.connect({
-//       host: hostName,
-//       username: username,
-//       privateKey: fs.readFileSync(privateKeyPath),
-//     });
-//   });
-
-//   // SCP each file/directory to the EC2 instance
-//   for (const itemName of filesToUpload) {
-//     const localPath = itemName;
-//     const remotePath = `/home/ubuntu/${itemName}`;
-
-//     console.log(`Uploading ${localPath} to ${remotePath}`);
-
-//     await new Promise((resolve, reject) => {
-//       sshClient.sftp((err, sftp) => {
-//         if (err) {
-//           sshClient.end();
-//           return reject(err);
-//         }
-//         sftp.fastPut(localPath, remotePath, (err) => {
-//           if (err) {
-//             return reject(err);
-//           }
-//           resolve();
-//         });
-//       });
-//     });
-//   }
-
-//   sshClient.end();
-//   console.log("Files uploaded successfully.");
-// }
-
 import fs from "fs";
 import path from "path";
 import { Client } from "ssh2";
-const DESTINATION_PATH = "/home/ubuntu/server";
+const DESTINATION_BASE_PATH = "/home/ubuntu/server";
+
+/**
+ * Goal : Recursively iterate through the current working directory and
+ *        create files / directories on the ec2 instance to mimic the current
+ *        working directory structure.
+ *
+ * Rules : Do not send over any files that end with .pem, or that are contained
+ *         within the node_modules directory, or package-lock.json
+ *
+ * upladFilesToEc2
+ * - Accepts connectionParams Object { hostName, username, privateKeyPath }
+ * - returns a  promise that resolves to undefined, or throws an error
+ *
+ * 1. Make a connection to the ec2 instance via SSH
+ *
+ * 2. Create a "server" directory on the ec2 instance
+ *    - createDir Helper function
+ *
+ * recursiveUpload
+ * - accepts a path to a directory
+ *
+ * 1. get a list of all the files in the directory
+ * 2. filter out any files / directories that should not be sent
+ * 3. iterate over the filtered list of entities
+ *    - If the entity is a file,
+ *        - send the file to ec2 at the correct path
+ *    - If the entity is a directory,
+ *        - pass the path to the directory + prev path as an argument to the
+ *          recursiveUpload method
+ *
+ *
+ */
 
 // Main function to upload files/directories to EC2
 export default async function uploadFilesToEC2(connectionParams) {
-  const { hostName, username, privateKeyPath } = connectionParams;
-  const localDir = process.cwd(); // Current working directory
-
-  // Get list of files and directories in the current working directory
-  const items = fs.readdirSync(localDir);
-
-  // Filter out node_modules and package-lock.json
-  const filesToUpload = items.filter(filterFunc);
-
-  console.log(filesToUpload);
-
   // Create SSH connection
-  const sshClient = new Client();
-  await new Promise((resolve, reject) => {
-    sshClient.on("error", reject);
-    sshClient.on("ready", resolve);
-    sshClient.connect({
-      host: hostName,
-      username: username,
-      privateKey: fs.readFileSync(privateKeyPath),
-    });
-  });
+  let sshClient = new Client();
+
+  await sshConnect(sshClient, connectionParams);
+
+  const sftp = await getSftpConnection(sshClient);
 
   console.log("Connected to EC2 instance.");
 
-  // Create server directory on the EC2 instance
-  await new Promise((resolve, reject) => {
+  // Create "server" directory on the EC2 instance
+  await createDir(sshClient, DESTINATION_BASE_PATH);
+  console.log("Created server directory on EC2 instance.");
+
+  // Upload each file/directory
+  const localDir = process.cwd();
+  const items = fs.readdirSync(localDir).filter(filterFunc);
+
+  for (const item of items) {
+    const localPath = path.join(localDir, item);
+    const remotePath = path.join(DESTINATION_BASE_PATH, item);
+
+    await uploadRecursive(localPath, remotePath, item, sshClient);
+  }
+  sshClient.end();
+}
+
+// Recursive function to upload files/directories
+const uploadRecursive = async (localPath, remotePath, item, sshClient) => {
+  console.log(`Processing ${localPath.split("/").pop()}`);
+  // Check if the item is a file or directory
+  const stats = fs.statSync(localPath);
+
+  console.log(
+    `File type of ${localPath.split("/").pop()}: ${
+      stats.isDirectory() ? "directory" : "file"
+    }`
+  );
+
+  if (stats.isDirectory()) {
+    // If it's a directory, create the directory on the remote server
+    await createDir(sshClient, remotePath);
+
+    // Read the directory and upload its contents recursively
+    const subItems = fs.readdirSync(localPath).filter(filterFunc);
+    for (const subItem of subItems) {
+      const subLocalPath = path.join(localPath, subItem);
+      const newRemotePath = path.join(remotePath, subItem);
+
+      await uploadRecursive(subLocalPath, newRemotePath, subItem, sshClient);
+    }
+  } else {
+    // If it's a file, upload it to the remote server
+    console.log(`Uploading ${localPath} to ${remotePath}`);
+    await sendFile(sshClient, localPath, remotePath);
+  }
+};
+
+async function createDir(sshClient, remotePath) {
+  return new Promise((resolve, reject) => {
     sshClient.sftp((err, sftp) => {
       if (err) {
+        sftp.end();
         return reject(err);
       }
-      sftp.mkdir(DESTINATION_PATH, (err) => {
+
+      // Check if the directory already exists
+      sftp.readdir(remotePath, (err) => {
+        if (!err) {
+          // Directory already exists, resolve immediately
+          console.log(`Directory ${remotePath} already exists`);
+          sftp.end();
+          return resolve();
+        }
+
+        // If readdir throws an error, it means the directory doesn't exist
+        // Attempt to create the directory
+        sftp.mkdir(remotePath, (err) => {
+          if (err) {
+            return reject(err);
+          }
+          console.log(`Created directory ${remotePath}`);
+          sftp.end();
+          resolve();
+        });
+      });
+    });
+  });
+}
+
+// // Refactor to use sftp connection
+// async function sendFile(sftp, localPath, remotePath) {
+//   console.log(`Sending file ${localPath} to ${remotePath}`);
+//   return await sftp.fastPut(localPath, remotePath, (err) =>
+//     err ? Promise.reject(err) : Promise.resolve()
+//   );
+// }
+
+// Send a file to the EC2 instance
+async function sendFile(sshClient, localPath, remotePath) {
+  console.log(`Sending file ${localPath} to ${remotePath}`);
+  return await new Promise((resolve, reject) => {
+    sshClient.sftp((err, sftp) => {
+      if (err) {
+        sftp.end();
+        return reject(err);
+      }
+      sftp.fastPut(localPath, remotePath, (err) => {
         if (err) {
+          sftp.end();
           return reject(err);
         }
+        sftp.end();
         resolve();
       });
     });
   });
-
-  // Upload each file/directory
-  for (const itemName of filesToUpload) {
-    const localPath = path.join(localDir, itemName);
-    const remotePath = "/home/ubuntu/server";
-
-    console.log(`Uploading ${localPath} to ${remotePath}`);
-
-    // Upload file or directory recursively
-    await uploadRecursive(localPath, remotePath, sshClient);
-  }
-
-  sshClient.end();
-  console.log("Files uploaded successfully.");
 }
 
 // Filter function to exclude node_modules and package-lock.json
@@ -171,52 +163,27 @@ const filterFunc = (fileName) => {
   );
 };
 
-// Recursive function to upload files/directories
-const uploadRecursive = async (localPath, remotePath, sshClient) => {
-  const stats = fs.statSync(localPath);
-
-  console.log(
-    `File type of ${localPath}: ${stats.isDirectory() ? "directory" : "file"}`
-  );
-
-  if (stats.isDirectory()) {
-    // If it's a directory, create the directory on the remote server
-    const newRemotePath = path.join(remotePath, path.basename(localPath));
-    await new Promise((resolve, reject) => {
-      sshClient.sftp((err, sftp) => {
-        if (err) {
-          return reject(err);
-        }
-        sftp.mkdir(newRemotePath, (err) => {
-          if (err) {
-            return reject(err);
-          }
-          resolve();
-        });
-      });
+// Connect the given sshClient to the server specified in the connectionParams
+async function sshConnect(sshClient, connectionParams) {
+  const { hostName, username, privateKeyPath } = connectionParams;
+  return await new Promise((resolve, reject) => {
+    sshClient.on("error", reject);
+    sshClient.on("ready", resolve);
+    sshClient.connect({
+      host: hostName,
+      username: username,
+      privateKey: fs.readFileSync(privateKeyPath),
     });
+  });
+}
 
-    // Read the directory and upload its contents recursively
-    const subItems = fs.readdirSync(localPath);
-    for (const subItem of subItems) {
-      const subLocalPath = path.join(localPath, subItem);
-      await uploadRecursive(subLocalPath, newRemotePath, sshClient);
-    }
-  } else {
-    // If it's a file, upload it to the remote server
-    const remoteFilePath = path.join(remotePath, path.basename(localPath));
-    await new Promise((resolve, reject) => {
-      sshClient.sftp((err, sftp) => {
-        if (err) {
-          return reject(err);
-        }
-        sftp.fastPut(localPath, remoteFilePath, (err) => {
-          if (err) {
-            return reject(err);
-          }
-          resolve();
-        });
-      });
-    });
+async function getSftpConnection(sshClient) {
+  try {
+    const sftp = await sshClient.sftp((err, sftp) =>
+      err ? Promise.reject(err) : Promise.resolve(sftp)
+    );
+    return sftp;
+  } catch (err) {
+    console.error("Error creating sftp connection:", err);
   }
-};
+}
